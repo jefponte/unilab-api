84ee0db461fae69edfb3a81e553d2a232bba48da
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-11-11 09:50:32 -0300

    Update laravel to laravel 11

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,7 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-
-$router->get('/', function () use ($router) {
-    return redirect('/docs/index.html');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-$router->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($router) {
-    $router->get('user', 'UserController@index');
-    $router->get('bond', 'UserController@bond');
-});
-
-$router->post('/api/authenticate', 'TokenController@generateToken');
-
-$router->post('/api/authenticate_md5', 'TokenController@generateTokenMd5');
