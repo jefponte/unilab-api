e67d7110dc9ed808ef46b027febbad4a5453452f
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-11-22 14:59:02 -0300

    Fix filters

--- app/ModelFilters/UserFilter.php
+++ app/ModelFilters/UserFilter.php
@@ -8,15 +8,27 @@ class UserFilter extends DefaultModelFilter
 
     public function name($name)
     {
-        $this->where('name', 'ILIKE', "%$name%");
+        if (env('DB_CONNECTION') === 'pgsql') {
+            $this->where('name', 'ILIKE', "%$name%");
+        } else {
+            $this->whereRaw("LOWER('name') LIKE ?", ['%' . strtolower($name) . '%']);
+        }
     }
     public function email($email)
     {
-        $this->where('email', 'ILIKE', "%$email%");
+        if (env('DB_CONNECTION') === 'pgsql') {
+            $this->where('email', 'ILIKE', "%$email%");
+        } else {
+            $this->whereRaw("LOWER('email') LIKE ?", ['%' . strtolower($email) . '%']);
+        }
     }
     public function login($login)
     {
-        $this->where('login', 'ILIKE', "%$login%");
+        if (env('DB_CONNECTION') === 'pgsql') {
+            $this->where('login', 'ILIKE', "%$login%");
+        } else {
+            $this->whereRaw("LOWER('login') LIKE ?", ['%' . strtolower($login) . '%']);
+        }
     }
 
     public function id($id)
